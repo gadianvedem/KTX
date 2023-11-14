b52dcdae571512199f082cb21dedab5acab6523a
Dinh <Dinh@COGAINAMAY>
2023-11-14 16:51:59 +0700

    Add project files.

---
+++ KTX/api.aspx.cs
@@ -0,0 +1,73 @@
+﻿using SuatAn;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace KTX
+{
+    public partial class api : System.Web.UI.Page
+    {
+        void sinhvien(string action)
+        {
+            SqlServer db = new SqlServer();
+            SqlCommand cm = db.GetCmd("SP_SinhVien", action);
+
+            switch (action)
+            {
+                case "add_company":
+                case "edit_company":
+
+                    cm.Parameters.Add("@masv", SqlDbType.VarChar, 20).Value = Request["masv"];
+                    cm.Parameters.Add("@hoten", SqlDbType.NVarChar, 20).Value = Request["hoten"];
+                    cm.Parameters.Add("@ngaysinh", SqlDbType.Date).Value = Request["ngaysinh"];
+                    cm.Parameters.Add("@gioitinh", SqlDbType.NVarChar, 5).Value = Request["gioitinh"];
+                    cm.Parameters.Add("@sdt", SqlDbType.VarChar, 15).Value = Request["sdt"];
+                    cm.Parameters.Add("@lop", SqlDbType.VarChar, 10).Value = Request["lop"];
+                    cm.Parameters.Add("@khoa", SqlDbType.NVarChar, 20).Value = Request["khoa"];
+                    break;
+            }
+
+
+            switch (action)
+            {
+                case "edit_company":
+                case "delete_company":
+                    // Add parameters for updating or deleting a book
+                    cm.Parameters.Add("@masv", SqlDbType.VarChar, 20).Value = Request["Masv"];
+                    break;
+            }
+
+
+
+            //thuc thi
+            string json = (string)db.Scalar(cm);
+            this.Response.Write(json);
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string action = Request["action"];
+
+            switch (action)
+            {
+                case "edit_company":
+                    sinhvien(action);
+                    break;
+                case "add_company":
+                    sinhvien(action);
+                    break;
+                case "list_company":
+                    sinhvien(action);
+                    break;
+
+                case "delete_company":
+                    sinhvien(action);
+                    break;
+            }
+        }
+    }
+}
---
+++ KTX/asset/mycode.js
@@ -0,0 +1,119 @@
+﻿$(document).ready(function () {
+    const api = '/api.aspx';
+    function delete_company(masv,json) {
+
+    }
+    function edit_company(masv, json) {
+        var sv;
+        for (var item of json.data) {
+            if (item.masv == masv) {
+                sv = item;
+                break;
+            }
+        }
+        var content = `form điền sẵn
+        Mã sinh viên :<input type=text id="edit-masinhvien" value="${sv.masv}"><br>
+        Họ và tên :<input type=text id="edit-tensinhvien" value="${sv.hoten}"><br>
+        Ngày sinh :<input type=date id="edit-ngaysinhsinhvien" value="${sv.ngaysinh}">
+        `
+        $.confirm({
+            title: 'edit sinh viên',
+            content: content,
+            columnClass: 'large',
+            buttons: {
+                save: {
+                    action: function () {
+                        var data_gui_di ={
+                            action: 'edit_company',
+                            masv : masv,
+                            masv: $('#edit-masinhvien').val(),
+                            hoten: $('#edit-tensinhvien').val(),
+                            ngaysinh: $('#edit-ngaysinhsinhvien').val()
+                        }
+                        /*console.log(data_gui_di);*/
+                        $.post(api, data_gui_di, function (data) {
+                            var json = JSON.parse(data);
+                            alert(json.msg);
+                        })
+                    }
+                },
+                cancel: {
+
+                }
+            }
+        })
+
+    }
+    function list_sinhvien() {
+        $.confirm({
+            title: "Danh sách sinh viên",
+            content: `<div id ="ds_sinh_vien">loading....</div>`,
+            columnClass: 'large',
+            onContentReady: function () {
+                //alert('oke rồi đấy')
+                $.post(api,
+                    {
+                        action: 'list_company'
+                    },
+                    function (data) {
+                        var json = JSON.parse(data);
+                        //alert(data)
+                        var noi_dung_ds_sinh_vien = "";
+                        var stt = 0;
+                        if (json.ok) {
+                            noi_dung_ds_sinh_vien += `<table class="table table-hover">"
+                            <thead>
+                            <tr>
+                                <th>STT</th>
+                                <th>Mã sinh viên</th>
+                                <th>Họ và tên</th>
+                                <th>Ngày sinh</th>
+                                <th>Giới tính</th>
+                                <th>SDT</th>
+                                <th>Lớp</th>
+                                <th>Khoa</th>
+                                <th>Sửa/Xóa</th>
+                            </tr>
+                            </thead><tbody>`;
+                            for (var sv of json.data) {
+                                var sua_xoa = `<button class="btn btn-sm btn-warning nut_sua_xoa" data-cid="${sv.masv}" data-action="edit_company">Sửa</button>`;
+                                sua_xoa += ` <button class="btn btn-sm btn-warning nut_sua_xoa" data-cid="${sv.masv}" data-action="delete_company">Xóa</button>`;
+                                noi_dung_ds_sinh_vien += `
+                                <tr>
+                                <td>${++stt}</td>
+                                <td>${sv.masv}</td>
+                                <td>${sv.hoten}</td>
+                                <td>${sv.ngaysinh}</td>
+                                <td>${sv.gioitinh}</td>
+                                <td>${sv.sdt}</td>
+                                <td>${sv.lop}</td>
+                                <td>${sv.khoa}</td>
+                                <td>${sua_xoa}</td>
+                            </tr>
+                                `
+                            }
+                            noi_dung_ds_sinh_vien += "</tbody></table>";
+                        }
+                        else {
+                            noi_dung_ds_sinh_vien += "không có dữ liệu";
+                        }
+
+                        $('#ds_sinh_vien').html(noi_dung_ds_sinh_vien);// gán html vào thân diaglo
+                        $('.nut_sua_xoa').click(function () {
+                            var action = $(this).data('action')
+                            var masv = $(this).data('cid')
+                            if (action == 'delete_company') {
+                                delete_company(masv, json);
+                            }
+                            else if (action == 'edit_company') {
+                                edit_company(masv, json);
+                            }
+                        });
+                    })
+            }
+        });
+    }
+    $('#nut-sinh-vien').click(function () {
+        list_sinhvien();
+    });
+});
